c990074dc4ff181b09bb154e4e83bd4354f99ce8
Mostafa Kamal <hiremostafa@gmail.com>
2024-02-20 03:15:01 +0600

    Revert "project"
    This reverts commit dc024f83ee1610f3ed6141488c7bd801d0e1af47.

--- backend/backend/models/project.py
+++ backend/backend/models/project.py
@@ -2,10 +2,10 @@ from . import db
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
-    project_path = db.Column(db.String(255), nullable=True)
-    template_name = db.Column(db.String(100), nullable=True)
-    dockerfile_path = db.Column(db.String(255), nullable=True)
+    name1 = db.Column(db.String(150), nullable=False)
+    project_path = db.Column(db.String(255), nullable=False)
+    template_name = db.Column(db.String(100), nullable=False)
+    dockerfile_path = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
-        return f"Project('{self.name}', '{self.project_path}', '{self.template_name}', '{self.dockerfile_path}')"
+        return f"Project('{self.name1}', '{self.project_path}', '{self.template_name}', '{self.dockerfile_path}')"
