c8d64ea97825d63d095cac401483cc0c41b4820f
Mostafa Kamal <hiremostafa@gmail.com>
2024-03-08 04:00:12 +0600

    ui fix

--- src/renderer/src/pages/ProjectDetails.tsx
+++ src/renderer/src/pages/ProjectDetails.tsx
@@ -67,7 +67,7 @@ function ProjectDetails(): JSX.Element {
         onDataFetch={handleDataFetch}
       />
 
-      <div className="flex min-h-[90vh] bg-gray-100 p-4 flex-col">
+      <div className="flex min-h-[90vh] max-h-[90vh] bg-gray-100 p-4 flex-col">
         <div className="mb-4">
           <p
             className="flex cursor-pointer text-teal-500 text-sm items-center"
@@ -77,8 +77,7 @@ function ProjectDetails(): JSX.Element {
             Back
           </p>
         </div>
-        <div className="mb-6 border-b-2 border-t-2 border-teal-500">
-          <p className="text-2xl font-bold mb-2">Project Details</p>
+        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-4 group hover:border-[1px] border-[1px] border-transparent">
           <p className="text-lg">Name: {theProjectData?.name}</p>
           <p className="text-lg">
             Path:{' '}
@@ -90,13 +89,13 @@ function ProjectDetails(): JSX.Element {
             </span>
           </p>
         </div>
-        <div className="mb-2 flex gap-2">
-          <p>dockerfile</p>
-          <p>nginx</p>
-          <p>supervisord</p>
-          <p>build image</p>
+        <div className="mb-4 flex gap-2 sticky top-0 bg-white p-2 mt-4">
+          <p className="border-b-2 border-teal-500 pb-1 cursor-pointer">dockerfile</p>
+          <p className="border-b-2 border-teal-500 pb-1 cursor-pointer">nginx</p>
+          <p className="border-b-2 border-teal-500 pb-1 cursor-pointer">supervisord</p>
+          <p className="border-b-2 border-teal-500 pb-1 cursor-pointer">build image</p>
         </div>
-        <div>
+        <div className="sticky top-0">
           <div>
             <button
               type="button"
@@ -113,42 +112,46 @@ function ProjectDetails(): JSX.Element {
               Save File
             </button>
           </div>
+          <hr className="mt-4" />
           <div className="flex w-full mt-2">
-            <div className="w-1/2 pr-2">
+            <div className="w-1/2  shadow  rounded-md bg-slate-200  overflow-y-auto max-h-[56vh]">
               {allDockerfiles?.map((stageItem: any) => (
-                <div key={stageItem.stage} className="mb-4">
+                <div
+                  key={stageItem.stage}
+                  className={`p-4 m-2 rounded shadow ${stageItem.dockerfiles.length > 0 ? 'bg-slate-100' : 'bg-slate-100'} mb-2`}
+                >
                   <p className="text-md font-bold mb-2">{stageItem.stage}</p>
                   <div className="flex flex-col">
                     {stageItem.dockerfiles.map((dockerfile) => (
                       <div
                         key={dockerfile.id}
-                        className="flex bg-white border cursor-move mb-2 w-full"
+                        className="mt-3 flex bg-white px-1 py-2 shadow sm:rounded-lg sm:px-1 group hover:border-[1px] border-[1px] border-transparent cursor-pointer"
                       >
                         <div className="py-2 px-4 flex-1">{dockerfile.method}</div>
-                        <div className="py-2 px-4 flex-2 ">
+                        <div className="py-2 px-4 flex-2">
                           {typeof dockerfile.data === 'string'
                             ? dockerfile.data
                             : JSON.stringify(dockerfile.data)}
                         </div>
                       </div>
                     ))}
-                    <div className="flex justify-end mb-2">
+                    <div className="flex justify-end mb-2 mt-2">
                       <button
-                        className="cursor-pointer border border-dotted border-red-500 px-2 py-1 rounded-md bg-teal-500 text-white"
+                        className="cursor-pointer rounded-md bg-teal-500 px-4 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         onClick={() => openDockerFileModal()}
                       >
-                        Add
+                        Add item
                       </button>
                     </div>
                   </div>
                 </div>
               ))}
             </div>
-            <div className="w-1/2 pl-2 mt-8">
+            <div className="w-1/2 pl-2">
               <textarea
                 disabled={false}
                 rows={25}
-                className="flex-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                className="flex-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm resize-none"
                 value={theDoc}
                 onInput={(event: any) => console.log('')}
                 placeholder={''}
