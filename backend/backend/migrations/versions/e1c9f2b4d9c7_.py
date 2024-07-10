3d25442312258e1cca269e15756fb2005914f958
Mostafa Kamal <hiremostafa@gmail.com>
2024-07-11 00:32:21 +0600

    migration

---
+++ backend/backend/migrations/versions/e1c9f2b4d9c7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e1c9f2b4d9c7
+Revises: 
+Create Date: 2024-07-11 00:29:38.125817
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1c9f2b4d9c7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
